8ac9db4a1caf4081022744e4bbba569399d19679
Yuly Villamil <villamilyuli69@gmail.com>
2021-08-29 01:52:15 -0500

    Eliminacion

--- inicio_1.php
+++ inicio_1.php
@@ -1,3 +1,3 @@
 <?php require_once "vistas/parte_superior.php"?>
-<img src="img/inicio.jpg" class="img-fluid" alt="Responsive image">
-<?php require_once "parte_inferior.php"?>
+hola mundo
+<?php require_once "vistas/parte_inferior.php"?>
