ce92392233233e5008dc2837aa33cbe30dc562f2
Yuly Villamil <villamilyuli69@gmail.com>
2021-08-29 01:19:24 -0500

    Se sube eliminacion de archivos

--- buttons.php
+++
@@ -1,5 +0,0 @@
-<?php require_once "vistas/parte_superior.php" ?>
-<!--/INICIO DE CONTENIDO PRINCIPAL-->
-<h1> contenido inferior <h1>
-<!--FIN DEL CONTENIDO PRINCIPAL-->
-<?php require_once "vistas/parte_inferior.php" ?>
